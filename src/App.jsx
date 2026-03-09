4d90ba88ea2c89ebbca41499c7d241eae5fb2384
dhinakaran-Y <dhinakaran.y.dhina14@gmail.com>
2026-03-09 09:14:11 +0530

    feat: collage cards updated, all collages data are gotten by fetch with promiseall method. loading , data loading progress bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
 import Home from './Components/Home'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
+import IndianColleges from './summa-2/IndianColleges'
+import CollegeApp from './summa-4/components/CollegeApp'
 
 function App() {
   
   return (
     <>
     <Header />
-     <Home />
+     {/* <CollegeApp/> */}
+     <Home/>
      <Footer />
     </>
   )
